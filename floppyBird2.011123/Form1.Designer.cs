0b046429b80494180da54feda8e08e3228a3a7bb
VS Coding <VS Coding@SDS-CODING>
2022-06-14 21:02:46 -0400

    Add project files.

---
+++ floppyBird2.011123/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace floppyBird2._011123
+{
+    partial class FloppyBird
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.subTitleLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // subTitleLabel
+            // 
+            this.subTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTitleLabel.Location = new System.Drawing.Point(41, 113);
+            this.subTitleLabel.Name = "subTitleLabel";
+            this.subTitleLabel.Size = new System.Drawing.Size(244, 111);
+            this.subTitleLabel.TabIndex = 5;
+            this.subTitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(231, 10);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(100, 23);
+            this.scoreLabel.TabIndex = 4;
+            this.scoreLabel.Text = "Score is ";
+            this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(106, 72);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(100, 23);
+            this.titleLabel.TabIndex = 3;
+            this.titleLabel.Text = "Floopy Bird";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FloppyBird
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(334, 486);
+            this.Controls.Add(this.subTitleLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FloppyBird";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FloppyBird_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FloppyBird_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.FloppyBird_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label subTitleLabel;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label titleLabel;
+    }
+}
+
---
+++ floppyBird2.011123/Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace floppyBird2._011123
+{
+    public partial class FloppyBird : Form
+    {
+        Image PGfloppy = Properties.Resources.PGfloppy;
+
+        Image backround = Properties.Resources.backround;
+
+        Image cloud = Properties.Resources.cloud;
+        List<Rectangle> movingBackroud = new List<Rectangle>();
+        int backroundSpeed = 2;
+        int backroundX = 334;
+        int backroundY = 243;
+        int backRoundCounter = 148;
+
+
+        List<Rectangle> clouds = new List<Rectangle>();
+        int cloudsSpeed = 1;
+        int cloudX = 47;
+        int cloudY = 30;
+        int cloudCounter = 0;
+
+
+        bool spaceBar = false;
+        bool pause = false;
+        bool quit = false;
+
+        int gameState = 1;
+        int tunnelCount = 0;
+        int speed = 4;
+
+        int score = 0;
+
+        int stateJump = 0;
+
+        Rectangle floppy = new Rectangle(50, 223, 25, 25);
+
+        Pen drawPen = new Pen(Color.Black, 5);
+        SolidBrush backFill = new SolidBrush(Color.Green);
+
+        List<Rectangle> topTunnel = new List<Rectangle>();
+        List<Rectangle> bottomTunnel = new List<Rectangle>();
+        List<Rectangle> INscore = new List<Rectangle>();
+
+        Random randGen = new Random();
+
+        public FloppyBird()
+        {
+            InitializeComponent();
+        }
+
+        public void GameInitialize()
+        {
+            gameState = 2;
+            score = 0;
+            tunnelCount = 0;
+            backRoundCounter = 148;
+            titleLabel.Visible = false;
+            subTitleLabel.Visible = false;
+
+            INscore.Clear();
+
+            floppy.Y = 223;
+
+            topTunnel.Clear();
+            bottomTunnel.Clear();
+
+
+            gameTimer.Start();
+            movingBackroud.Clear();
+            clouds.Clear();
+
+            movingBackroud.Add(new Rectangle(0, 333, backroundX, backroundY));
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FloppyBird_Paint(object sender, PaintEventArgs e)
+        {
+            if (gameState == 1)
+            {
+
+            }
+            else if (gameState == 2)
+            {
+                for (int i = 0; i < movingBackroud.Count; i++)
+                {
+                    e.Graphics.DrawImage(backround, movingBackroud[i]);
+                }
+                for (int i = 0; i < clouds.Count; i++)
+                {
+                    e.Graphics.DrawImage(cloud, clouds[i]);
+                }
+                e.Graphics.DrawImage(PGfloppy, floppy);
+                //(backFill, floppy);
+                //   score = score / 12; 
+                scoreLabel.Text = $"Score is {score}";
+
+                for (int i = 0; i < topTunnel.Count; i++)
+                {
+                    e.Graphics.FillRectangle(backFill, topTunnel[i]);
+                }
+                for (int i = 0; i < bottomTunnel.Count; i++)
+                {
+                    e.Graphics.FillRectangle(backFill, bottomTunnel[i]);
+                }
+
+            }
+            else
+            {
+                subTitleLabel.Text = $"";
+
+            }
+        }
+
+        private void FloppyBird_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Q:
+                    quit = true;
+
+                    if (gameState == 1 || gameState == 3)
+                    {
+                        this.Close();
+                    }
+                    break;
+                case Keys.Space:
+                    if (gameState == 1 || gameState == 3)
+                    {
+                        GameInitialize();
+
+                    }
+                    spaceBar = true;
+                    break;
+                case Keys.P:
+                    pause = true;
+                    break;
+            }
+        }
+
+        private void FloppyBird_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Q:
+                    quit = false;
+                    break;
+                case Keys.Space:
+                    spaceBar = false;
+                    break;
+                case Keys.P:
+                    pause = false;
+                    break;
+
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            // Charcter Movement 
+            if (spaceBar == true)
+            {
+                stateJump = 1;
+            }
+            // jump anamtion 
+            switch (stateJump)
+            {
+                case 1:
+                    floppy.Y -= 10;
+                    stateJump++;
+                    break;
+                case 2:
+                    floppy.Y -= 5;
+                    stateJump++;
+                    break;
+                case 3:
+                    floppy.Y -= 2;
+                    stateJump++;
+                    break;
+                case 4:
+                    floppy.Y -= 0;
+                    stateJump++;
+                    break;
+                case 5:
+                    floppy.Y += 2;
+
+                    stateJump++;
+                    break;
+                case 6:
+                    floppy.Y += 6;
+                    stateJump = 0;
+                    break;
+                default:
+                    floppy.Y += 6;
+                    stateJump = 0;
+                    break;
+            }
+            // check if flooppy hit the bottom 
+
+            if (floppy.Y > 476)
+            {
+                gameState = 3;
+            }
+
+            // Creating the tunnels if it is time 
+
+            tunnelCount++;
+
+            if (tunnelCount == 70)
+            {
+                int Random = randGen.Next(1, 350);
+                topTunnel.Add(new Rectangle(400, 0, 30, Random));
+                INscore.Add(new Rectangle(400, Random + 5, 5, 100));
+
+                //     pointList.Add(new bool (true)); 
+                int hight = 486 - Random - 100;
+                Random = Random + 100;
+                bottomTunnel.Add(new Rectangle(400, Random, 30, hight));
+
+                tunnelCount = 0;
+
+
+            }
+
+
+            ////Moving the tunnes
+
+            for (int i = 0; i < topTunnel.Count; i++)
+            {
+                int x = topTunnel[i].X - speed;
+                topTunnel[i] = (new Rectangle(x, 0, 30, topTunnel[i].Height));
+            }
+            for (int i = 0; i < bottomTunnel.Count; i++)
+            {
+                int x = bottomTunnel[i].X - speed;
+                bottomTunnel[i] = (new Rectangle(x, bottomTunnel[i].Y, 30, bottomTunnel[i].Height));
+            }
+
+            for (int i = 0; i < INscore.Count; i++)
+            {
+                int x = INscore[i].X - speed;
+                INscore[i] = (new Rectangle(x, INscore[i].Y, 30, INscore[i].Height));
+            }
+
+            // removing the tunnels 
+
+            for (int i = 0; i < topTunnel.Count; i++)
+            {
+                if (topTunnel[i].X <= -20)
+                {
+                    topTunnel.RemoveAt(i);
+
+                }
+                if (bottomTunnel[i].X <= -20)
+                {
+                    bottomTunnel.RemoveAt(i);
+                }
+
+
+
+            }
+
+
+            // intercetion of the tunnels
+
+            for (int i = 0; i < bottomTunnel.Count; i++)
+            {
+                if (floppy.IntersectsWith(bottomTunnel[i]))
+                {
+                    gameState = 3;
+                }
+                if (floppy.IntersectsWith(topTunnel[i]))
+                {
+                    gameState = 3;
+                }
+            }
+
+
+            // scoring 
+
+            for (int i = 0; i < INscore.Count; i++)
+            {
+                if (floppy.IntersectsWith(INscore[i]))
+                {
+                    score++;
+                    INscore.RemoveAt(i);
+                    scoreLabel.Text = $"score is {score}";
+
+                }
+
+
+            }
+            // city moving backround 
+
+            backRoundCounter++;
+            if (backRoundCounter == 149)
+            {
+                movingBackroud.Add(new Rectangle(300, 333, backroundX, backroundY));
+                backRoundCounter = 0;
+            }
+            for (int i = 0; i < movingBackroud.Count; i++)
+            {
+                int x = movingBackroud[i].X - backroundSpeed;
+                movingBackroud[i] = new Rectangle(x, movingBackroud[i].Y, backroundX, backroundY);
+            }
+
+
+            // removing ctiys for the list. 
+            for (int i = 0; i < movingBackroud.Count; i++)
+
+            {
+                if (movingBackroud[i].X <= -400)
+                {
+                    movingBackroud.RemoveAt(i);
+                }
+            }
+
+
+            // clouds moving backround 
+            cloudCounter++;
+            if (cloudCounter == 100)
+            {
+                int Random = randGen.Next(1, 350);
+
+                clouds.Add(new Rectangle(334, Random, cloudX, cloudY));
+                cloudCounter = 0;
+            }
+
+            for (int i = 0; i < clouds.Count; i++)
+            {
+                int x = clouds[i].X - cloudsSpeed;
+                clouds[i] = new Rectangle(x, clouds[i].Y, cloudX, cloudY);
+            }
+
+            // removing clouds 
+
+
+            for (int i = 0; i < clouds.Count; i++)
+
+            {
+                if (clouds[i].X <= -40)
+                {
+                    clouds.RemoveAt(i);
+                }
+            }
+
+
+            Refresh();
+        }
+    }
+}
