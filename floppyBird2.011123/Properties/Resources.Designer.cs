2fe944b242593b3deaf3ac50bb8bcfca8982f346
VS Coding <VS Coding@SDS-CODING>
2022-06-15 13:47:47 -0400

    ok

--- floppyBird2.011123/Properties/Resources.Designer.cs
+++ floppyBird2.011123/Properties/Resources.Designer.cs
@@ -81,6 +81,16 @@ namespace floppyBird2._011123.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] Kahoot_Lobby_Music {
+            get {
+                object obj = ResourceManager.GetObject("Kahoot_Lobby_Music", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap PGfloppy {
